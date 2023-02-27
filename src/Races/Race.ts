ab5e5eaa9d15e80162f2de3fb802b8f2c9d712d2
Karin Melo <karinsantosmelo@gmail.com>
2023-02-27 16:28:51 -0300

    Criação da classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  readonly name: string;
+  readonly _dexterity: number;
+  
+  constructor(name: string, dexterity: number) {
+    this.name = name;
+    this._dexterity = dexterity;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  get dexterity() : number {
+    return this._dexterity;
+  }
+
+  public static createdRacesInstances() : number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get maxLifePoints() : number;
+}
+
+export default Race;
